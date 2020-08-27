e7759992c93ac5dbc716cc9a7a12c630911d9f3c
AViteDiaz18 <a.vitediaz18@gmail.com>
2020-08-27 13:31:12 -0500

    cambios

--- AreaTriangulo/AreaTriangulo.cs
+++ AreaTriangulo/AreaTriangulo.cs
@@ -9,9 +9,9 @@ namespace AreaTriangulo
             double Base = 0, altura = 0, area = 0;
             Console.WriteLine("Calcula el area de un triangulo");
             Console.Write("Ingrese la base del triangulo: ");
-            Base = Int32.Parse(Console.ReadLine());
+            Base = float.Parse(Console.ReadLine());
             Console.Write("Ingrese la altura del triangulo: ");
-            altura = Int32.Parse(Console.ReadLine());
+            altura = float.Parse(Console.ReadLine());
             area = (Base * altura)/2;
             Console.WriteLine("El area del triangulo es : " + area);
         }
