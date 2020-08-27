ec28d0e1d5310c5783dcb417ea842e9d6bbcb091
AViteDiaz18 <a.vitediaz18@gmail.com>
2020-08-27 13:29:31 -0500

    cambios

--- AreaCirculo/Program.cs
+++ AreaCirculo/Program.cs
@@ -9,8 +9,8 @@ namespace AreaCirculo
             double area = 0, radio = 0;
             Console.WriteLine("Calcula el area de un circulo");
             Console.Write("Ingrese el radio del circulo: ");
-            radio = Int32.Parse(Console.ReadLine());
-            area = Math.PI * radio * radio;
+            radio = float.Parse(Console.ReadLine());
+            area = Math.PI * Math.Pow(radio,2);
             Console.WriteLine("El area del circulo es : " + area);
         }
     }
