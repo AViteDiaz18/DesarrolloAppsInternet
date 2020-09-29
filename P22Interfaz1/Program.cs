ce7dca97a08053154dc595b711c8665970b614bf
AViteDiaz18 <a.vitediaz18@gmail.com>
2020-09-29 13:00:33 -0500

    cambios

---
+++ P22Interfaz1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P22Interfaz1
+{
+    class Program
+    {
+        public interface IAnimal{
+            string nombre {get;set;}
+            void llorar();
+        }
+
+        class Perro : IAnimal{
+            public Perro(string ndog) => nombre = ndog;
+            public string nombre {get;set;}
+            public void llorar() => Console.WriteLine("Woff Woff");
+        }
+
+        class Gato : IAnimal{
+            public Gato(string ncat) => nombre = ncat;
+            public string nombre {get;set;}
+            public void llorar() => Console.WriteLine("Miau Miau");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primer ejemplo de uso de interfaces");
+            
+            Perro miperro = new Perro("Sabueso");
+            Console.WriteLine($"El perro {miperro.nombre}");
+            miperro.llorar();
+
+            Gato migato = new Gato("Misifu");
+            Console.WriteLine($"El gato {migato.nombre}");
+            migato.llorar();
+        }
+    }
+}
---
+++ P23Interfaz2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace P23Interfaz2
+{
+    public class Organismo{
+        public void Respiracion() => Console.WriteLine("Respirando");
+        public void Movimiento() => Console.WriteLine("Moviendose");
+        public void Crecimiento() => Console.WriteLine("Creciendo");
+    }
+    public interface IAnimales{
+        void Cromosomas();
+        void Temperatura();
+    }
+    public interface ICuadrupedo : IAnimales{
+        void Correr();
+        void CuatroPatas();
+    }
+
+    public interface IAves : IAnimales{
+        void Volar();
+        void DosPatas();
+    }
+    public class Perro : Organismo, ICuadrupedo{
+        public void Cromosomas()=> Console.WriteLine("El perro tiene 39 pares cromosomas");
+        public void Temperatura()=> Console.WriteLine("El perro esta a una temperatura de 38 grados");
+        public void Correr()=> Console.WriteLine("Puede Correr");
+        public void CuatroPatas()=> Console.WriteLine("Tiene 4 patas");
+    }
+    public class Perico : Organismo, IAves{
+        public void Cromosomas()=> Console.WriteLine("El perico tiene 46 pares cromosomas");
+        public void Temperatura()=> Console.WriteLine("El perico esta a una temperatura de 27 grados");
+        public void Volar()=> Console.WriteLine("Puede Volar");
+        public void DosPatas()=> Console.WriteLine("Tiene 2 Patas");
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Segundo ejemplo de interfaces:\n********************");
+
+            Perro miperro = new Perro();
+            Console.WriteLine("Caracteristicas del perro:");
+            miperro.Respiracion();
+            miperro.Movimiento();
+            miperro.Crecimiento();
+            miperro.Cromosomas();
+            miperro.Temperatura();
+            miperro.CuatroPatas();
+            miperro.Correr();
+
+            Perico miperico = new Perico();
+            Console.WriteLine("\nCaracteristicas del perico:");
+            miperico.Respiracion();
+            miperico.Movimiento();
+            miperico.Crecimiento();
+            miperico.Cromosomas();
+            miperico.Temperatura();
+            miperico.DosPatas();
+            miperico.Volar();
+        }
+    }
+}
